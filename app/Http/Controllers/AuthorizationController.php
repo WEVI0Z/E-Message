683db31809214502705c373a1aabad6fb468ff80
Wevioz <wevioz@mail.ru>
2023-04-21 08:38:59 +0300

    user model created

--- app/Http/Controllers/AuthorizationController.php
+++ app/Http/Controllers/AuthorizationController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorizationController extends Controller
 {
@@ -13,11 +17,43 @@ class AuthorizationController extends Controller
         ]);
     }
 
-    function register(Request $request) {
-        $request->validate([
-            "name" => "required",
-            "password" => "required|min:8",
-            "password-repeat" => "required",
+    function loginApi(Request $request) {
+        $validator = Validator::make($request->all(), [
+            "login" => "required",
+            "password" => "required",
         ]);
+
+        if($validator->fails()) {
+            return response([
+                'status' => 'Validator exception',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        return response(["access_token" => User::query()->where("login", "=", $request->login)->where("password", "=", $request->password)]->get()->access_token, 200);
+    }
+
+    function registerApi(Request $request) {
+        $validator = Validator::make($request->all(), [
+            "login" => "required|unique:users",
+            "password" => "required|min:8",
+        ]);
+
+        if($validator->fails()) {
+            return response([
+                'status' => 'Validator exception',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $token = Hash::make($request->login);
+
+        User::create([
+            "login" => $request->login,
+            "password" => $request->password,
+            "access_token" => $token
+        ]);
+
+        return response(["access_token" => $token], 200);
     }
 }
